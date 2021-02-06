c6ce3e77135bc92ad3ea5d72e35477c8d79458e6
Imani Dap <imani.dap@gmail.com>
2021-02-06 15:55:57 +0100

    add player specific sounds

--- game/game/sound.ino
+++ game/game/sound.ino
@@ -1,4 +1,29 @@
+//converted notes to frequencies with help of https://github.com/tsukisan/Arduino/blob/master/WiiClassicSoundboard/Notes.h
 const int buzzerPin = 3;
+
+void playPlayerEastSound() {
+  int frequencies[] = {349, 523, 349, 1047};
+  int durations[] = {125, 375, 125, 375};
+
+  for(int i = 0; i < 4; i++) {
+    int freq = frequencies[i];
+    int duration = durations[i];
+    tone(buzzerPin, freq, duration);
+    delay(duration);
+  }
+}
+
+void playPlayerWestSound() {
+  int frequencies[] = {262, 523, 1319, 2093};
+  int durations[] = {125, 375, 125, 375};
+
+  for(int i = 0; i < 4; i++) {
+    int freq = frequencies[i];
+    int duration = durations[i];
+    tone(buzzerPin, freq, duration);
+    delay(duration);
+  }
+}
 
 void playScore(int firstScore, int secondScore) {
   int scores[] = {firstScore, secondScore};
